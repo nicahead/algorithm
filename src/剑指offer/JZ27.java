ccbd16f81e6769f8cdffda365178b10792cfd8ae
nicahead <nicahead@gmail.com>
2021-03-18 20:53:28 +0800

    剑指 Offer 27. 二叉树的镜像

---
+++ src/剑指offer/JZ27.java
@@ -0,0 +1,24 @@
+package 剑指offer;
+
+//剑指 Offer 27. 二叉树的镜像
+public class JZ27 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+        return root;
+    }
+}
