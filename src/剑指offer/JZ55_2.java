9b656c35b4b2f5fc461b157e08c08dbc5318aaf7
nicahead <nicahead@gmail.com>
2021-03-24 19:13:14 +0800

    剑指 Offer 55 - II. 平衡二叉树

---
+++ src/剑指offer/JZ55_2.java
@@ -0,0 +1,28 @@
+package 剑指offer;
+
+/*
+剑指 Offer 55 - II. 平衡二叉树
+输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+ */
+public class JZ55_2 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        if (Math.abs(getDepth(root.left) - getDepth(root.right)) > 1) return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
+    }
+}
